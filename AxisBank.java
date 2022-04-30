9151c4b6bfeaec439e4ad0c36179ba708bf3d9f7
Sreelekshmi91 <102054755+Sreelekshmi91@users.noreply.github.com>
2022-05-01 01:25:07 +0530

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,19 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	public void deposit() {
+
+		System.out.println("Minimum deposit of Rs 2,00,00,000 with no maximum limit on deposits");
+	}
+
+	public static void main(String[] args) {
+
+		BankInfo bankInfo = new BankInfo();
+		bankInfo.saving();
+		bankInfo.fixed();
+		bankInfo.deposit();
+
+		AxisBank axisBank = new AxisBank();
+		axisBank.deposit();
+	}
+}
